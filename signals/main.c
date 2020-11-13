6495ad422fa758137b8a7ad22d4fe7c90c7eaad4
dmitrryy <da.drozdov@mail.ru>
2020-11-13 14:11:15 +0300

    signals done

--- signals/main.c
+++ signals/main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -17,23 +18,34 @@ int getBit(char sim_, int num_);
 void nop() {}
 
 int cur_bit_ = 0;
-int go_next_ = 0;
 
-void receivedUSR1(int i) {
-    cur_bit_ = 0;
-    if (go_next_ == 1) {
-        printf("err go_next_\n");
-        exit(1);
+
+void SigHandler(int sig_)
+{
+    switch(sig_)
+    {
+        case SIGUSR1:
+            cur_bit_ = 0;
+            break;
+
+        case SIGUSR2:
+            cur_bit_ = 1;
+            break;
+
+        case SIGCHLD: {
+            int status__ = 0;
+            wait(&status__);
+            if (status__ != 0) {
+                printf("child died\n");
+                exit(1);
+            }
+            break;
+        }
+
+        case SIGTERM:
+            printf("terminate\n");
+            exit(2);
     }
-    go_next_ = 1;
-}
-void receivedUSR2(int i) {
-    cur_bit_ = 1;
-    if (go_next_ == 1) {
-        printf("err go_next_\n");
-        exit(1);
-    }
-    go_next_ = 1;
 }
 
 //it doesn't work correctly
@@ -51,23 +63,29 @@ int main (int argc, char* argv[])
 
 
     int df_source = open(argv[1], O_RDONLY);
-    if (df_source == -1) 
+    if (df_source == -1)
     {
         printf ("error: failed open source file: %s\n", argv[1]);
         exit(2);
     }
 
     struct sigaction sa = {};
-    sa.sa_handler = receivedUSR1;
+    sa.sa_handler = SigHandler;
     sigaction(SIGUSR1, &sa, NULL);
-    sa.sa_handler = receivedUSR2;
     sigaction(SIGUSR2, &sa, NULL);
+    sigaction(SIGCHLD, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+
+    sigset_t sproc_mask;
+    sigemptyset(&sproc_mask);
+    sigaddset(&sproc_mask, SIGUSR1);
+    sigaddset(&sproc_mask, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &sproc_mask, NULL);
 
     int ppid = getpid();
     int is_parent = fork();
     if (is_parent == 0)
-    {
-        //is child
+    {//is child
         if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
         {
             printf("ch: error prctl()\n");
@@ -80,85 +98,82 @@ int main (int argc, char* argv[])
         }
 
         char cur_s = 0;
-        sigset_t mask;
-        sigemptyset(&mask);
-        //sigdelset(&mask, SIGUSR1);
-        //sigdelset(&mask, SIGUSR2);
-        //sigdelset(&mask, SIGTERM);
+
+        sigset_t ssus_mask;
+        sigfillset(&ssus_mask);
+        sigdelset(&ssus_mask, SIGUSR1);
+        sigdelset(&ssus_mask, SIGTERM);
+
         while(read(df_source, &cur_s, sizeof(char)) > 0)
         {
-            //sleep(1);
-            for(int i = 0; i < 8; i++)
+
+            for (int i = 0; i < 8; i++)
             {
-                sigsuspend(&mask);
-                int bit = getBit(cur_s, i);                
+                int bit = getBit(cur_s, i);
 
                 if (bit == 0) {
-                    if (kill(ppid, SIGUSR1) == -1) {
-                        printf("err\n");
-                    }
+                    kill(ppid, SIGUSR1);
                 }
                 else {
-                    if (kill(ppid, SIGUSR2) == -1) {
-                        printf("err\n");
-                    }
+                    kill(ppid, SIGUSR2);
                 }
-                if (go_next_ != 1) {//wrong
-                    sigsuspend(&mask);
-                }
-                go_next_ = 0;
+                sigsuspend(&ssus_mask);
             }
         }
-        //sleep(1);
-        for(int i = 0; i < 8; i++)
+
+        cur_s = EOF;
+        for (int i = 0; i < 8; i++)
         {
-            kill(ppid, SIGUSR1);
-            if (go_next_ != 1) {
-                sigsuspend(&mask);
+            int bit = getBit(cur_s, i);
+
+            if (bit == 0) {
+                kill(ppid, SIGUSR1);
             }
-            go_next_ = 0;
+            else {
+                kill(ppid, SIGUSR2);
+            }
+            sigsuspend(&ssus_mask);
         }
+
         return 0;
     }
     else if (is_parent > 0)
-    {
-        //is parent
-        sigset_t mask;
-        sigemptyset(&mask);
-        //sigdelset(&mask, SIGUSR1);
-        //sigdelset(&mask, SIGUSR2);
-        //sigdelset(&mask, SIGTERM);
-        //sigdelset(&mask, SIGSTOP);
+    {//is parent
+        sigset_t ssus_mask;
+        sigfillset(&ssus_mask);
+        sigdelset(&ssus_mask, SIGUSR1);
+        sigdelset(&ssus_mask, SIGUSR2);
+        sigdelset(&ssus_mask, SIGTERM);
+        sigdelset(&ssus_mask, SIGCHLD);
+
         char cur_sim = 0;
         do {
             cur_sim = 0;
+
             for (int i = 0; i < 8; i++)
             {
-                if (kill(is_parent, SIGUSR1) == -1) {
-                    printf("err\n");
-                }
-                if (go_next_ != 1) {
-                    //a signal can come here and that's it all
-                    sigsuspend(&mask);
-                }
-                cur_sim = cur_sim | (cur_bit_ << i);
-                go_next_ = 0;
-                if (kill(is_parent, SIGUSR1) == -1) {
-                    printf("err\n");
-                }
-            }
-            printf ("%c", cur_sim);
-            fflush(0);
+                sigsuspend(&ssus_mask);
+                kill(is_parent, SIGUSR1);
 
-        } while (cur_sim != 0);
+                cur_sim = cur_sim | (cur_bit_ << i);
+            }
+            if (cur_sim != EOF) {
+                printf("%c", cur_sim);
+                fflush(0);
+            }
+
+        } while (cur_sim != EOF);
+
         printf("\n");
+
+        waitpid(is_parent, NULL, 0);
     }
     else
     {
         printf("fork error\n");
         exit(1);
     }
-    
+
 
     return 0;
 }
@@ -167,5 +182,5 @@ int main (int argc, char* argv[])
 int getBit(char sim_, int num_)
 {
     if (num_ > 7 || num_ < 0) { return 0;}
-    return (sim_ & (1 << num_)) != 0; 
+    return (sim_ & (1 << num_)) != 0;
 }
