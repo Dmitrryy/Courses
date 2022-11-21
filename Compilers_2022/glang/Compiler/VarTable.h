3ed3c90f343ffa4346f5ccf6a5c1e93d210f83da
Dmitrryy <da.drozdov@mail.ru>
2022-11-21 12:37:16 +0300

    [Compiler_2022] VarTable refactored

--- Compilers_2022/glang/Compiler/VarTable.h
+++ Compilers_2022/glang/Compiler/VarTable.h
@@ -2,139 +2,98 @@
 
 #include <unordered_map>
 #include <vector>
-#include <cassert>
-#include <optional>
 #include <stack>
-#include <memory>
 
 namespace ezg
 {
-    template< typename Key_t, typename Val_t >
+    /// @brief Implements a tree of scopes.
+    /// @tparam Key_t - id type. 
+    /// The type must meet the 'key' requirements in std::unordered_map.
+    /// @tparam Val_t - value type.
+    template <typename Key_t, typename Val_t>
     class ScopeTable final
     {
-
-        class Node_t final
+        struct Node_t final
         {
-            Node_t *m_parent = nullptr;
-            std::vector<Node_t *> m_children;
-
+            size_t m_parent = -1;
             std::unordered_map<Key_t, Val_t> m_data;
-
-        public:
-            void setParent(Node_t *pNode) { m_parent = pNode; }
-
-            void addChild(Node_t *pNode)
-            {
-                pNode->setParent(this); /**/
-                m_children.push_back(pNode);
-            }
-
-            void declare(const Key_t &id, const Val_t &data)
-            {
-                assert(m_data.count(id) == 0);
-                m_data[id] = std::move(data);
-            }
-
-            Val_t &access(const Key_t &id)
-            {
-                if (m_data.count(id)) {
-                    return access_cur_(id);
-                }
-
-                if (m_parent != nullptr)
-                {
-                    return m_parent->access(id);;
-                }
-
-                throw std::runtime_error("Undeclared id!");
-            }
-
-            bool isDeclared(const Key_t &id) const
-            {
-                bool res = isDeclared_cur_(id);
-                if(!res && m_parent != nullptr) {
-                    res = m_parent->isDeclared(id);
-                }
-                return res;
-            }
-
-        private:
-            Val_t &access_cur_(const Key_t &id)
-            {
-                return m_data.at(id);
-            }
-
-            bool isDeclared_cur_(const Key_t &id) const
-            {
-                return m_data.count(id);
-            }
         }; // class Node_t
 
-        std::vector<Node_t *> m_tables;
+        std::vector<Node_t> m_tables;
         std::stack<size_t> m_scopeIdStore;
 
     public:
-        ScopeTable() = default;
+        /// @brief Entering the table with the id number.
+        /// The id is placed at the top of the stack. When exit_scope is called, 
+        /// the table will return to the previous scope (and not to the parent of cur scope).
+        /// @param id - table identifier.
+        void entryScope(size_t id) { m_scopeIdStore.push(id); }
 
-        // not supported
-        ScopeTable(const ScopeTable &) = delete;
-        const ScopeTable &operator=(const ScopeTable &) = delete;
-        ScopeTable(ScopeTable &&) = delete;
-        const ScopeTable &operator=(ScopeTable &&) = delete;
+        /// @brief In stack order returns to the previous table.
+        void exitCurScope() { m_scopeIdStore.pop(); }
 
-        ~ScopeTable()
+        /// @brief 
+        /// @return identifier of cur table.
+        size_t getCurTableId() const { return m_scopeIdStore.top(); }
+
+        /// @brief Crete new scope table.
+        /// @param prevId - parent scope. All objects from prevId table are visible
+        /// from new table. If prevId is -1, than new table is not conneted to any old table.
+        /// @return id of new table.
+        size_t createTable(size_t prevId = -1)
         {
-            for (const auto &pN : m_tables)
+            if (prevId != -1 && prevId >= m_tables.size())
             {
-                delete pN;
+                throw std::out_of_range("prevId greaater than number of tables!");
             }
-        }
-
-        void entryScope(size_t id)
-        {
-            assert(id < m_tables.size());
-            m_scopeIdStore.push(id);
-        }
-        void exitCurScope()
-        {
-            m_scopeIdStore.pop();
-        }
-
-        size_t getCurTableId() const noexcept { return m_scopeIdStore.top(); }
-
-        size_t createTable(size_t prevId)
-        {
-            assert(prevId < m_tables.size());
             size_t newId = m_tables.size();
-            auto *newNode = new Node_t();
+
+            Node_t newNode;
+            newNode.m_parent = prevId;
+
             m_tables.push_back(newNode);
-
-            newNode->setParent(m_tables[prevId]);
-            m_tables[prevId]->addChild(newNode);
-
             return newId;
         }
 
-        size_t createTable()
+        /// @brief return data of VISIBLE object with 'id'
+        Val_t &access(const Key_t &id)
         {
-            size_t newId = m_tables.size();
-            auto *newNode = new Node_t();
-
-            m_tables.push_back(newNode);
-
-            return newId;
+            size_t cur_id = m_scopeIdStore.top();
+            while (cur_id != -1)
+            {
+                auto &&table = m_tables.at(cur_id);
+                if (table.m_data.count(id))
+                {
+                    return table.m_data.at(id);
+                }
+                cur_id = table.m_parent;
+            }
+            throw std::runtime_error("Undeclared id!");
         }
 
-        Val_t& access(const Key_t &id) const
+        /// @brief return true if 'id' is visible at 'idTable' table.
+        /// If there is not 'id' at 'idTable' and 'idTable' connected to 'prevId' (see createTable),
+        /// than algorithm search 'id' at 'prevId'.
+        bool isDeclared(size_t idTable, const Key_t &id) const
         {
-            return m_tables[m_scopeIdStore.top()]->access(id);
+            bool result = false;
+            while (idTable != -1 && !result)
+            {
+                auto &&table = m_tables.at(idTable);
+                result = table.m_data.count(id);
+                idTable = table.m_parent;
+            }
+            return result;
         }
 
-        bool isDeclared(size_t idTable, const Key_t &id) const {
-            return m_tables.at(idTable)->isDeclared(id);
+        /// @brief Declare new object at certain scope
+        /// @param idTable - scop where object were declared
+        /// @param idElem - identifier of new element
+        /// @param data - value of new element
+        void declare(size_t idTable, const Key_t &idElem, const Val_t &data)
+        {
+            m_tables[idTable].m_data[idElem] = data;
         }
-
-        void declare(size_t idTable, const Key_t &idElem, const Val_t &data) { m_tables[idTable]->declare(idElem, data); }
     }; // class ScopeTable
 
 } // namespace ezg
